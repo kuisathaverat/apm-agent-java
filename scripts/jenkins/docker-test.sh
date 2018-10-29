1a905621f7f0ddec4151ca6efe5cfaf43b360413
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2018-10-29 12:52:19 +0100

    make docker tests

--- scripts/jenkins/docker-test.sh
+++ scripts/jenkins/docker-test.sh
@@ -8,15 +8,20 @@ go get -v -u github.com/matm/gocov-html
 go get -v -u github.com/axw/gocov/...
 go get -v -u github.com/AlekSi/gocov-xml
 
-go get -v -t ./... || echo "KO"
+go get -v -t ./...
 
 export COV_FILE="build/coverage.cov"
 export OUT_FILE="build/test-report.out"
 mkdir -p build
 
 ./scripts/docker-compose-testing up -d --build
-./scripts/docker-compose-testing run -T --rm go-agent-tests make coverage > ${COV_FILE}
-cat ${OUT_FILE} | go-junit-report > build/apm-agent-go-junit.xml
+./scripts/docker-compose-testing run -T --rm go-agent-tests make coverage | tee ${COV_FILE}.raw
+echo "mode: atomic" > ${COV_FILE}
+grep -v "mode\: atomic" ${COV_FILE}.raw >> ${COV_FILE}
 gocov convert "${COV_FILE}" | gocov-html > build/coverage-apm-agent-go-report.html
 gocov convert "${COV_FILE}" | gocov-xml > build/coverage-apm-agent-go-report.xml
+
+./scripts/docker-compose-testing run -T --rm go-agent-tests go test -race ./... -v 2>&1 | tee ${OUT_FILE}
+cat ${OUT_FILE} | go-junit-report > build/apm-agent-go-junit.xml
+
 
